847c0bdb63d04076d0149f4a1acd45a02224c643
ganghun <ghjeong908@gmail.com>
2017-07-19 03:30:59 +0900

    7/19 project commit

---
+++ P2_JGH2/src/BookRegistor_Dialog.java
@@ -0,0 +1,17 @@
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+
+public class BookRegistor_Dialog extends Dialog {
+
+	public BookRegistor_Dialog(Shell parent, int style) {
+		super(parent, style); 
+		setText("SWT Dialog");
+	}
+
+	public void open() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
